f8537d05051eed0f05d3c1141e5117e374f0181b
이준형 <50432821+with-developer@users.noreply.github.com>
2023-04-10 23:19:28 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,17 +1,11 @@
 def solution(a, b):
     answer = 0
     
-    if(a>b): # b가 작으면? 
+    if(a>b):
         temp_b = b
         b = a 
         a = temp_b
     
-    sub = b-a
     answer = sum(range(a, b + 1))
 
-
-    
-
-
-# a=-10, b=-8이면 -27이 나와야함.
     return answer
