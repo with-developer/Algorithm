a5a14a8816c423a5a0325d7935b43d95ceb06f37
이준형 <50432821+with-developer@users.noreply.github.com>
2023-04-10 19:29:35 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,18 +1,14 @@
 import math
 
-def solution(n):
-    # 제곱인지 아닌지 판단? 121은 11의 제곱인걸 어떻게 알 수 있지?
-    # 1안. 무작정 1부터 큰 수까지 제곱해서 if문 돌리기. 제일 비효율적임.
-    # 2안. 역제곱?을 계산하는 법이 있나?
-    # 121이 들어오면? 이게 11의 제곱이구나 하는걸 어떻게 알수있지?
-    # math의 sqrt함수.
-    
+def solution(n):    
+    # math 라이브러리의 sqrt 함수는 제곱근을 출력
+    # pow 함수는 제곱을 출력
     answer = 0
-    reverse = math.sqrt(n)
+    n_sqrt = math.sqrt(n)
     
-    if (reverse % 1 != 0):
+    if (n_sqrt % 1 != 0):
         answer = -1
     else:
-        answer = math.pow(int(reverse)+1,2)
+        answer = math.pow(int(n_sqrt)+1,2)
 
     return answer
