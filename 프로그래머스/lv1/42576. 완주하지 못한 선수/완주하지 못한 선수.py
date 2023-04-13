e9d53450df6419e658cec7a6304353a4deead8de
이준형 <50432821+with-developer@users.noreply.github.com>
2023-04-13 23:04:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.52 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+    answer = ''
+    none_count = 0
+    
+    participant.sort()
+    completion.sort()
+    # print(participant)
+    # print(completion)    
+
+    for i in range(len(completion)):
+        if (participant[i] != completion[i]):
+            answer = participant[i]
+            break
+            
+    if (answer == ""):
+        answer = participant[-1]
+    
+    return answer
