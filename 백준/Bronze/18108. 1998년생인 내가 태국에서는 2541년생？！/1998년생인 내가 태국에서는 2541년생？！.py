43f60c547781919316e44aa88c0e8f6f175a9e19
이준형 <50432821+with-developer@users.noreply.github.com>
2023-08-04 09:39:49 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.py
@@ -0,0 +1,12 @@
+import sys
+
+#A, B, C = map(int, sys.stdin.readline().split())
+
+n = int(input())
+
+
+print(n-543)
+
+# 2541 1998       
+#첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
