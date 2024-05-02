6cea22762245de94c09e033be56d1d643ca1d585
이준형 (Harry) <50432821+with-developer@users.noreply.github.com>
2024-05-02 17:48:59 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+if score <= 100 and score >=90:
+    print("A")
+elif score <=89 and score >=80:
+    print("B")
+elif score <=79 and score >=70:
+    print("C")
+elif score <= 69 and score >= 60:
+    print("D")
+else:
+    print("F")
