db203692c784b979a6f72093b4619552dd72214c
이준형 (Harry) <50432821+with-developer@users.noreply.github.com>
2024-07-01 09:25:20 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+n, k = map(int, input().split())
+print(factorial(n) // (factorial(k) * factorial(n-k)))
